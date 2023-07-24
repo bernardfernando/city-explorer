608c9c1c4e66af93a0668fa9c2715a00c4a457bf
bernardfernando <jagathf@gmail.com>
2023-07-24 15:12:12 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+import { useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return <div className="App">City explorer</div>;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState({});
+  const [map, setMap] = useState("");
+
+  function handleSearch(event) {
+    setSearchQuery(event.target.value);
+    console.log(searchQuery);
+  }
+
+  async function getLocation(event) {
+    try {
+      event.preventDefault();
+      event.target.input.value = "";
+      const API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json`;
+      console.log(API);
+      const res = await axios.get(API);
+      handleMap(res.data[0]);
+    } catch (error) {}
+
+    function handleMap(data) {
+      const API = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${data.lat},${data.lon}&zoom=9`;
+      setMap(API);
+    }
+  }
+  return (
+    <div className="App">
+      <form onSubmit={getLocation}>
+        <input
+          type="text"
+          placeholder="search for a city"
+          onChange={handleSearch}
+          name="input"
+        />
+        <button type="submit">Explorer</button>
+      </form>
+      <p>{location.display_name}</p>
+      {map && <img src={map} alt="map" />}
+    </div>
+  );
 }
 
 export default App;
